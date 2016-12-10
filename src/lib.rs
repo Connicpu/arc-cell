933d90519aaf6ea44329728360977846df5414ca
Connor Hilarides <connorcpu@live.com>
2016-12-10 01:50:11 -0800

    Bump for documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::sync::{Arc, RwLock, Weak};
 
+/// A Cell for containing a strong reference
 pub struct ArcCell<T> {
     inner: RwLock<Arc<T>>,
 }
@@ -12,44 +13,52 @@ impl<T> ArcCell<T> {
         }
     }
 
-    /// 
+    /// Get the pointer contained in this cell as it exists at this moment
     pub fn get(&self) -> Arc<T> {
         let inner = self.inner.read().expect("It should have been impossible for this to get poisoned");
         (*inner).clone()
     }
 
+    /// Set the pointer for the next observer
     pub fn set(&self, value: Arc<T>) {
         let mut inner = self.inner.write().expect("It should have been impossible for this to get poisoned");
         *inner = value;
     }
 }
 
+/// A Cell for containing a weak reference
 pub struct WeakCell<T> {
     inner: RwLock<Weak<T>>,
 }
 
 impl<T> WeakCell<T> {
+    /// Construct the Cell with an empty Weak pointer. Upgrading this
+    /// value will always return None.
     pub fn empty() -> WeakCell<T> {
         WeakCell {
             inner: RwLock::new(Weak::new())
         }
     }
 
+    /// Get the Weak pointer as it is at this moment
     pub fn get(&self) -> Weak<T> {
         let inner = self.inner.read().expect("It should have been impossible for this to get poisoned");
         (*inner).clone()
     }
 
+    /// Try to upgrade the Weak pointer as it is now into a Strong pointer
     pub fn upgrade(&self) -> Option<Arc<T>> {
         let inner = self.inner.read().expect("It should have been impossible for this to get poisoned");
         inner.upgrade()
     }
 
+    /// Set a Weak pointer you currently have as the pointer in this cell
     pub fn set(&self, value: Weak<T>) {
         let mut inner = self.inner.write().expect("It should have been impossible for this to get poisoned");
         *inner = value;
     }
 
+    /// Downgrade a Strong pointer and store it in the cell
     pub fn store(&self, value: &Arc<T>) {
         self.set(Arc::downgrade(&value));
     }
