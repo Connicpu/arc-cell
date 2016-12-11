f302886edd0b2866ae9dcba2efc72094d0293c51
Connor Hilarides <connorcpu@live.com>
2016-12-10 23:18:08 -0800

    Use a spinlock instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,31 @@
-use std::sync::{Arc, RwLock, Weak};
+extern crate spin;
+
+use spin::Mutex;
+use std::sync::{Arc, Weak};
 use std::fmt;
 
 /// A Cell for containing a strong reference
 pub struct ArcCell<T> {
-    inner: RwLock<Arc<T>>,
+    inner: Mutex<Arc<T>>,
 }
 
 impl<T> ArcCell<T> {
     /// Constructs an ArcCell which initially points to `value`
     pub fn new(value: Arc<T>) -> ArcCell<T> {
         ArcCell {
-            inner: RwLock::new(value)
+            inner: Mutex::new(value)
         }
     }
 
     /// Get the pointer contained in this cell as it exists at this moment
     pub fn get(&self) -> Arc<T> {
-        let inner = self.inner.read().expect("It should have been impossible for this to get poisoned");
+        let inner = self.inner.lock();
         (*inner).clone()
     }
 
     /// Set the pointer for the next observer
     pub fn set(&self, value: Arc<T>) {
-        let mut inner = self.inner.write().expect("It should have been impossible for this to get poisoned");
+        let mut inner = self.inner.lock();
         *inner = value;
     }
 }
@@ -35,7 +38,7 @@ impl<T: fmt::Debug> fmt::Debug for ArcCell<T> {
 
 /// A Cell for containing a weak reference
 pub struct WeakCell<T> {
-    inner: RwLock<Weak<T>>,
+    inner: Mutex<Weak<T>>,
 }
 
 impl<T> WeakCell<T> {
@@ -43,25 +46,25 @@ impl<T> WeakCell<T> {
     /// value will always return None.
     pub fn empty() -> WeakCell<T> {
         WeakCell {
-            inner: RwLock::new(Weak::new())
+            inner: Mutex::new(Weak::new())
         }
     }
 
     /// Get the Weak pointer as it is at this moment
     pub fn get(&self) -> Weak<T> {
-        let inner = self.inner.read().expect("It should have been impossible for this to get poisoned");
+        let inner = self.inner.lock();
         (*inner).clone()
     }
 
     /// Try to upgrade the Weak pointer as it is now into a Strong pointer
     pub fn upgrade(&self) -> Option<Arc<T>> {
-        let inner = self.inner.read().expect("It should have been impossible for this to get poisoned");
+        let inner = self.inner.lock();
         inner.upgrade()
     }
 
     /// Set a Weak pointer you currently have as the pointer in this cell
     pub fn set(&self, value: Weak<T>) {
-        let mut inner = self.inner.write().expect("It should have been impossible for this to get poisoned");
+        let mut inner = self.inner.lock();
         *inner = value;
     }
 
